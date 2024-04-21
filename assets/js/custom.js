5c9175017658f2144dd6421db40ba34fcf7b5cdd
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2024-04-21 11:22:19 +0600

    upload

---
+++ assets/js/custom.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    "use strict";
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        loop:true,
+        nav:true,
+        dots:true,
+        
+    });
+    // $('.our_client_content').slick({
+    //     arrows: true,
+    //     centerPadding: "0px",
+    //     dots: true,
+    //     slidesToShow: 1,
+    //     infinite: false
+    //   });
+  });
+
+// $(function() {
+//     // Owl Carousel
+//     var owl = $(".owl-carousel");
+//     owl.owlCarousel({
+//       items: 1,
+//       loop: true,
+//       nav: true,
+//       margin: 10,
+//     });
+//   });
