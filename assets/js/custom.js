eeb7a644750d640cba65d449ac7d9ae07df38e7f
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2024-04-21 13:00:16 +0600

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -26,3 +26,51 @@ $(document).ready(function(){
 //       margin: 10,
 //     });
 //   });
+
+var menu_count = 0;
+$(document).on('click','.solutions_menu',function(){
+  if(menu_count == 0){
+    $('.mega_menu').show();
+    $(this).addClass('active'); 
+    menu_count = 1
+  }else{ 
+    $('.mega_menu').hide();
+    $(this).removeClass('active'); 
+    menu_count = 0
+  }
+  return false;
+});
+
+var menu_count1 = 0;
+$(document).on('click','.drop_menu',function(){
+  if(menu_count1 == 0){
+    $('.dorp_down').show();
+    $(this).addClass('active'); 
+    menu_count1 = 1
+  }else{ 
+    $('.dorp_down').hide();
+    $(this).removeClass('active'); 
+    menu_count1 = 0
+  }
+
+  return false;
+});
+
+
+var menu_count2 = 0;
+$(document).on('click','.click_bars',function(){ 
+
+    if(menu_count2 == 0){
+      $('.menu ul').show(); 
+      $('.click_bars').html('<i class="fa-solid fa-xmark"></i>'); 
+      menu_count2 = 1
+    }else{ 
+      $('.menu ul').hide(); 
+      $('.mega_menu').hide();
+      $('.dorp_down').hide();
+      $('.click_bars').html('<i class="fas fa-bars"></i>'); 
+      menu_count2 = 0
+    }
+
+    return false;
+});
