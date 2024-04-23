40b252a7a0ce6bf17f90f3c67f6bbd97b53a5534
Sadbin Shakil <71561507+Shakil7979@users.noreply.github.com>
2024-04-23 18:13:41 +0600

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -28,11 +28,15 @@ $(document).ready(function(){
 //   });
 
 var menu_count = 0;
+var menu_count1 = 0;
 $(document).on('click','.solutions_menu',function(){
   if(menu_count == 0){
     $('.mega_menu').show();
     $(this).addClass('active'); 
-    menu_count = 1
+    $('.drop_menu').removeClass('active'); 
+    $('.dorp_down').hide();
+    menu_count = 1;
+    menu_count1 = 0;
   }else{ 
     $('.mega_menu').hide();
     $(this).removeClass('active'); 
@@ -41,12 +45,14 @@ $(document).on('click','.solutions_menu',function(){
   return false;
 });
 
-var menu_count1 = 0;
 $(document).on('click','.drop_menu',function(){
   if(menu_count1 == 0){
     $('.dorp_down').show();
+    $('.mega_menu').hide();
+    $('.solutions_menu').removeClass('active'); 
     $(this).addClass('active'); 
-    menu_count1 = 1
+    menu_count1 = 1;
+    menu_count = 0
   }else{ 
     $('.dorp_down').hide();
     $(this).removeClass('active'); 
@@ -61,11 +67,11 @@ var menu_count2 = 0;
 $(document).on('click','.click_bars',function(){ 
 
     if(menu_count2 == 0){
-      $('.menu ul').show(); 
+      $('.menu_mobile_last').show(); 
       $('.click_bars').html('<i class="fa-solid fa-xmark"></i>'); 
       menu_count2 = 1
     }else{ 
-      $('.menu ul').hide(); 
+      $('.menu_mobile_last').hide(); 
       $('.mega_menu').hide();
       $('.dorp_down').hide();
       $('.click_bars').html('<i class="fas fa-bars"></i>'); 
@@ -74,3 +80,30 @@ $(document).on('click','.click_bars',function(){
 
     return false;
 });
+
+
+$(document).ready(function() {
+  // Initially hide collapsible menus
+  $('.collaps_menu').hide();
+
+  // Event delegation to handle clicks on menu items with sub-menus
+  $('.menu_ul > li > a').click(function(event) {
+      // Only process clicks for devices under 992px
+      if ($(window).width() < 992) {
+          // Prevent default link behavior
+          event.preventDefault();
+          
+          // Toggle the next 'ul' element within the same parent 'li'
+          $(this).next('ul').slideToggle('slow'); // Use slideToggle with 'slow' speed
+      }
+  });
+  
+  // Ensure that submenus are not visible when resizing to larger viewports
+  $(window).resize(function() {
+      if ($(window).width() >= 992) {
+          $('.menu_ul ul').css('display', ''); // Reset to default CSS instead of show/hide to respect CSS rules
+      } else {
+          $('.menu_ul ul').slideUp('slow'); // Use slideUp with 'slow' speed
+      }
+  });
+}); 
